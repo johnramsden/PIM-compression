ae894badbec8a6443e56e2b8dd72d9fb24aa8724
John Ramsden <johnramsden@riseup.net>
2020-06-01 00:09:11 -0700

    Add configuration file, use pathlib
    Review for UBC-ECE-Sasha/PIM-compression#9
    
    Config:
    
    Rather than hardcoding configuration in the script, using a
    configuration file would allow for easier changes.
    
    It would also allow ignoring of the real configuration file,
    so that multiple people could use different configuration files.
    An example of a configuration file could contain what is usually
    used.
    
    Paths:
    
    Using raw string paths is fraught with problems. In the
    current implementation if one uses the path 'out' as the directory,
    a failure occurs since the directory is just appended to
    the file name.
    
    pathlib allows paths to be treated as objects, and correctly joined.
    
    Opening files:
    
    Currently open files are not properly closed. Use a context
    manager to correctly manage the resource and close it when the
    resource manager block has exited.
    
    Division by zero:
    
    Currently if the files are missing that are in the 'files' dict,
    division by zero occurs.
    
    Emit a warning that no files were found, rather than an exception
    occurring.

--- snappy/scripts/dpu_tasklet_tradeoff.py
+++ snappy/scripts/dpu_tasklet_tradeoff.py
@@ -1,65 +1,94 @@
 import os
 import sys
 import argparse
+import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
-from parse_output_file import get_avg_max_cycles 
+from parse_output_file import get_avg_max_cycles
 
 
-# Get the output file directory path
-parser = argparse.ArgumentParser(description='Create graph of cycle count by DPU and tasklets')
-requiredArgs = parser.add_argument_group('required arguments')
-requiredArgs.add_argument('PATH', help='directory holding output files to parse')
-requiredArgs.add_argument('FILENAME', help='test file name without file ending')
+def setup_graph(cfg: dict):
+    """
+    Setup and show the Graph
+    :param cfg: Config data
+    """
+    idx = 0
+    cycles = []
+    for tasks in cfg.get("num_tasks"):
+        # 2D list of size num_tasks by num_dpus
+        cycles.append([0] * len(cfg.get("num_dpus")))
 
-args = parser.parse_args()
-path = args.PATH
-testfile = args.FILENAME
-if not os.path.isdir(path):
-	raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+        dpu_idx = 0
+        for dpus in cfg.get("num_dpus"):
+            cycles[idx][dpu_idx] = get_avg_max_cycles(
+                cfg.get("path"), cfg.get("testfile"), dpus, tasks) / 1000000
+            dpu_idx += 1
+        idx += 1
 
-# Loop through directory for respective output files and parse them
-num_dpus = ['16', '32', '64', '128']
-num_tasks = ['4', '8', '12', '16', '20', '24']
+    # Print for easy debugging
+    print(cycles)
 
-idx = 0
-cycles = []
-for tasks in num_tasks:
-	# 2D list of size num_tasks by num_dpus
-	cycles.append([0] * len(num_dpus))
+    # Set up plot
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=12)
+    plt.rc('axes', labelsize=12)
+    fig, ax = plt.subplots()
 
-	dpu_idx = 0;
-	for dpus in num_dpus:
-		cycles[idx][dpu_idx] = get_avg_max_cycles(path, testfile, dpus, tasks) / 1000000
-		dpu_idx += 1
-	idx += 1	
+    # Set up positions of each bar
+    ypos = []
+    width = 0.13
+    ypos.append(np.arange(len(cfg.get("num_dpus"))))
+    for i in range(len(cfg.get("num_tasks")) - 1):
+        ypos.append([x + width for x in ypos[i]])
 
-# Print for easy debugging
-print(cycles)
+    # Set up axes labels
+    plt.xticks([r + width for r in range(len(cfg.get("num_dpus")))], cfg.get("num_dpus"))
+    ax.set_xlabel('Number of DPUs')
+    ax.set_ylabel('Cycle Count (in Millions)')
 
-# Set up plot
-colors = ['#c9b395', '#5c4832', '#2e3d18', '#4e6625', '#80be1b', '#cbd970'] # Need as many as num_tasks
-plt.rc('font', size=12)
-plt.rc('axes', titlesize=12)
-plt.rc('axes', labelsize=12)
-fig, ax = plt.subplots()
+    for i in range(0, len(cfg.get("num_tasks"))):
+        ax.bar(ypos[i], cycles[i], color=cfg.get("colors")[i], width=width, edgecolor='white')
 
-# Set up positions of each bar
-ypos = []
-width = 0.13
-ypos.append(np.arange(len(num_dpus)))
-for i in range (0, len(num_tasks) - 1):
-	ypos.append([x + width for x in ypos[i]])
+    ax.legend([x + ' Tasklets' for x in cfg.get("num_tasks")])
 
-# Set up axes labels
-plt.xticks([r + width for r in range(len(num_dpus))], num_dpus)
-ax.set_xlabel('Number of DPUs')
-ax.set_ylabel('Cycle Count (in Millions)')
+    plt.show()
 
-for i in range (0, len(num_tasks)):
-	ax.bar(ypos[i], cycles[i], color=colors[i], width=width, edgecolor='white')
 
-ax.legend([x + ' Tasklets' for x in num_tasks])
+if __name__ == "__main__":
+    color_default = '#c9b395'
+    try:
+        import config
+    except ImportError as err:
+        print("Config file 'config.py' does not exist", file=sys.stderr)
+        exit(1)
 
-plt.show()
+    # Get the output file directory path
+    parser = argparse.ArgumentParser(description='Create graph of cycle count by DPU and tasklets')
+    requiredArgs = parser.add_argument_group('required arguments')
+    requiredArgs.add_argument('PATH', help='directory holding output files to parse')
+    requiredArgs.add_argument('FILENAME', help='test file name without file ending')
 
+    args = parser.parse_args()
+    path = pathlib.Path(args.PATH)
+    testfile = args.FILENAME
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+
+    conf = {
+        "num_tasks": config.num_tasks,
+        "num_dpus": config.num_dpus,
+        "colors": config.colors,
+        "testfile": testfile,
+        "path": path
+    }
+
+    color_len = len(conf.get("colors"))
+    task_len = len(conf.get("num_tasks"))
+    if len(conf.get("num_tasks")) > color_len:
+        print("WARNING: num_tasks is larger than colors.\n"
+              f"{color_default} will be used for remaining colors.", file=sys.stderr)
+        conf["colors"] = [
+            conf["colors"][i] if i < color_len else color_default for i in range(task_len)
+        ]
+
+    setup_graph(conf)
--- snappy/scripts/host_speedup.py
+++ snappy/scripts/host_speedup.py
@@ -1,73 +1,101 @@
-import os
-import sys
 import argparse
+import pathlib
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
-from parse_output_file import get_avg_max_cycles, get_avg_host_runtime 
+from parse_output_file import get_avg_max_cycles, get_avg_host_runtime
 
 
-# Get the output file directory path
-parser = argparse.ArgumentParser(description='Create graph of DPU speedup over host')
-requiredArgs = parser.add_argument_group('required arguments')
-requiredArgs.add_argument('PATH', help='directory holding output files to parse')
+def setup_graph(cfg: dict):
+    """
+    Setup and show the Graph
+    :param cfg: Config data
+    """
+    # Loop through directory for respective output files and parse them
+    cycles = []
+    host_time = []
+    for filename in cfg.get("files"):
+        params = cfg.get("files")[filename]
 
-args = parser.parse_args()
-path = args.PATH
-if not os.path.isdir(path):
-	raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+        ahr = get_avg_host_runtime(cfg.get("path"), filename)
+        amc = get_avg_max_cycles(cfg.get("path"), filename, params[0], params[1])
 
-	   # 'test file' : ('num_dpus', 'num_tasklets')
-files = {'terror2': ('1',   '4'), 
-        'plrabn12': ('2',   '8'), 
-		'world192': ('4',   '12'),
-		'xml'     : ('15',  '12'), 
-		'sao'     : ('21',  '12'),
-		'dickens' : ('35',  '12'),
-		'nci'     : ('64',  '18'), 
-		'mozilla' : ('105', '16'), 
-		'spamfile': ('172', '16')}
+        if ahr is None:
+            print(f"WARNING: No file {filename} was found for host, skipping.",
+                  file=sys.stderr)
 
-# Loop through directory for respective output files and parse them
-cycles = []
-host_time = []
-for filename in files:
-	params = files[filename]
- 
-	host_time.append(get_avg_host_runtime(path, filename))
-	cycles.append(get_avg_max_cycles(path, filename, params[0], params[1]))
+        if amc is None:
+            print(f"WARNING: No file {filename} was found for cycles, skipping.",
+                  file=sys.stderr)
 
-# Calculate the speedup
-speedup = []
-for i in range (0, len(files)):
-	dpu_time = float(cycles[i]) / 267000000 # DPU clock speed i 267MHz
+        host_time.append(ahr)
+        cycles.append(amc)
 
-	if host_time[i] < dpu_time:
-		speedup.append((host_time[i] / dpu_time - 1) * 100)
-	else:
-		speedup.append((host_time[i] / dpu_time) * 100)
+    # Calculate the speedup
+    speedup = []
+    for i in range(len(cfg.get("files"))):
+        if cycles[i] is None or host_time[i] is None:
+            continue
 
-# Print for easy debugging
-print(files)
-print(speedup)
+        dpu_time = float(cycles[i]) / 267000000  # DPU clock speed i 267MHz
 
-# Set up plot
-plt.rc('font', size=12)
-plt.rc('axes', titlesize=12)
-plt.rc('axes', labelsize=12)
-fig, ax = plt.subplots()
+        if host_time[i] < dpu_time:
+            speedup.append((host_time[i] / dpu_time - 1) * 100)
+        else:
+            speedup.append((host_time[i] / dpu_time) * 100)
 
-# y-axis labels
-yticks = np.arange(len(files))
-ax.set_yticks(yticks)
-ax.set_yticklabels(files)
+    # Print for easy debugging
+    print(cfg.get("files"))
+    print(speedup)
 
-# x-axis labels
-xticks = np.arange(-100, 800, step=50)
-ax.set_xticks(xticks)
-ax.set_xlabel('Speedup Over Host Application (%)')
-ax.xaxis.grid(True, linestyle="dotted")
+    # Set up plot
+    plt.rc('font', size=12)
+    plt.rc('axes', titlesize=12)
+    plt.rc('axes', labelsize=12)
+    fig, ax = plt.subplots()
 
-ax.barh(yticks, speedup, color=list(map(lambda x: '#d35e60' if (x < 0) else '#84ba5b', speedup)))
+    # y-axis labels
+    yticks = np.arange(len(cfg.get("files")))
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(cfg.get("files"))
 
-plt.show()
+    # x-axis labels
+    xticks = np.arange(-100, 800, step=50)
+    ax.set_xticks(xticks)
+    ax.set_xlabel('Speedup Over Host Application (%)')
+    ax.xaxis.grid(True, linestyle="dotted")
 
+    if len(speedup) > 0:
+        ax.barh(yticks, speedup,
+                color=list(map(lambda x: '#d35e60' if (x < 0) else '#84ba5b', speedup)))
+
+        plt.show()
+    else:
+        print(f"ERROR: No data found.", file=sys.stderr)
+        exit(1)
+
+
+if __name__ == "__main__":
+    try:
+        import config
+    except ImportError as err:
+        print("Config file 'config.py' does not exist", file=sys.stderr)
+        exit(1)
+
+    # Get the output file directory path
+    parser = argparse.ArgumentParser(description='Create graph of DPU speedup over host')
+    requiredArgs = parser.add_argument_group('required arguments')
+    requiredArgs.add_argument('PATH', help='directory holding output files to parse')
+
+    args = parser.parse_args()
+    path = pathlib.Path(args.PATH)
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+
+    conf = {
+        "path": path,
+        "files": config.files
+    }
+
+    setup_graph(conf)
